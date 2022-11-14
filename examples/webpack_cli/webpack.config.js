10595bde436196d26b706bff0b4125b953c645b5
Raymond Carino <ray.carino@glean.com>
2022-11-14 01:14:32 -0800

    copy

--- examples/webpack_cli/webpack.config.js
+++ examples/webpack_cli/webpack.config.js
@@ -1,16 +1,47 @@
-const path = require('path')
+const path = require('path');
+const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const out_path = path.resolve(__dirname, 'dist')
-
-module.exports = (webpackEnv = {}) => {
-    return {
-        entry: path.join(__dirname, 'index.js'),
-        // stats: 'verbose',
-        mode: 'development',
-        stats: 'detailed',
-        output: {
-            filename: 'main.js',
-            path: out_path,
-        },
-    }
-}
+module.exports = {
+  entry: path.join(__dirname, './src/index.tsx'),
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.json', '.ts', '.tsx'],
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              presets: [
+                '@babel/preset-typescript',
+                ['@babel/preset-react', { runtime: 'automatic' }],
+                [
+                  '@babel/preset-env',
+                  { targets: { node: 14 }, modules: false },
+                ],
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new VanillaExtractPlugin(),
+  ],
+  stats: 'minimal',
+};
